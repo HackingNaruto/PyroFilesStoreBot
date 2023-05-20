94b3ceb60ba1466b51f9cf94cb30bc8d7bd4759d
ImAbduAllah <124421420+ImAbduAllah@users.noreply.github.com>
2023-05-20 11:29:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -9,7 +9,7 @@ class Config(object):
 	BOT_TOKEN = "6064443781:AAF2bSDg_XEperrhs5SUEQZDcrXf8V2uAIc"
 	BOT_USERNAME = "FilesStoreOfficialBot"
 	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001707804911"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1278414182"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1994781564"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://video:video@rishi.fzpls33.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "TamilLinksOfficial")
 	LOG_CHANNEL = -1001707804911
